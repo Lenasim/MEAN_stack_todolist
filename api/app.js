f5fc30fdf454b4ecd05ada32c9474fae5700d911
Lenasim <radical.rin@gmail.com>
2020-06-03 22:58:26 +0200

    changing code for CORS error

--- api/app.js
+++ api/app.js
@@ -8,8 +8,13 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 //cors headers middleware
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+        res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
+        return res.status(200).json({});
+    };
     next();
 });
 //Load mongoose model
