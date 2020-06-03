0fb86961d4ec137ae94ce206487334b062009236
Lenasim <radical.rin@gmail.com>
2020-06-03 22:23:12 +0200

    CRUD task added

--- api/app.js
+++ api/app.js
@@ -36,22 +36,58 @@ app.patch('/lists/:id', (req, res) => {
     List.findOneAndUpdate({ _id: req.params.id }, {
         $set: req.body
     })
-    .then(() => {
-        res.sendStatus(200);
-    })
+        .then(() => {
+            res.sendStatus(200);
+        })
 });
 
 app.delete('/lists/:id', (req, res) => {
-    List.findOneAndRemove({ 
-        _id: req.params.id
+    List.findOneAndRemove({ _id: req.params.id })
+        .then(removedListDoc => {
+            res.send(removedListDoc);
+        })
+});
+
+app.get('/lists/:listId/tasks', (req, res) => {
+    Task.find({ _listId: req.params.listId })
+        .then(tasks => {
+            res.send(tasks);
+        })
+});
+
+app.post('/lists/:listId/tasks', (req, res) => {
+    let newTask = new Task({
+        title: req.body.title,
+        _listId: req.params.listId
+    });
+    newTask.save()
+        .then(newTaskDoc => {
+            res.send(newTaskDoc);
+        })
+});
+
+app.patch('/lists/:listId/tasks/:taskId', (req, res) => {
+    Task.findOneAndUpdate({
+        _id: req.params.taskId,
+        _listId: req.params.listId
+    }, {
+        $set: req.body
+    })
+        .then(() => {
+            res.sendStatus(200);
+        })
+})
+
+app.delete('/lists/:listId/tasks/:taskId', (req, res) => {
+    Task.findOneAndRemove({
+        _id: req.params.taskId,
+        _listId: req.params.listId
     })
     .then(removedListDoc => {
         res.send(removedListDoc);
     })
 });
 
-
-
 app.listen(8000, () => {
     console.log('server is listening on port 8000')
-})
+});
